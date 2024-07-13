7de613e01daac9bab74ed330a05116c3294548d9
Arto Bendiken <arto@bendiken.net>
2024-07-14 03:20:33 +0400

    Simplify conditional compilation.

--- lib/protoflow/src/feature.rs
+++ lib/protoflow/src/feature.rs
@@ -1,4 +1,7 @@
 // This is free and unencumbered software released into the public domain.
 
 #[allow(unused)]
-pub static FEATURES: &[&str] = &[];
+pub static FEATURES: &[&str] = &[
+    #[cfg(feature = "tracing")]
+    "tracing",
+];
