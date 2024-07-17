4449379e07b2175b44ed3f5d4abcd1eddde437ac
Arto Bendiken <arto@asimov.so>
2024-07-17 12:24:48 +0900

    Differentiate between input/output ports.

--- lib/protoflow/src/block.rs
+++ lib/protoflow/src/block.rs
@@ -1,6 +1,21 @@
 // This is free and unencumbered software released into the public domain.
 
+use crate::{InputPort, OutputPort};
+//use prost::Message;
+
 #[allow(unused)]
-pub trait Block {
+pub trait Block: AsBlock {
+    fn inputs(&self) -> Vec<InputPort>;
+    fn outputs(&self) -> Vec<OutputPort>;
     fn execute(&mut self);
+}
+
+pub trait AsBlock {
+    fn as_block(&self) -> &dyn Block;
+}
+
+impl<T: Sized + Block> AsBlock for T {
+    fn as_block(&self) -> &dyn Block {
+        self
+    }
 }
