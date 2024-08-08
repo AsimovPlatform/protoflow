5c1849a728504ca1970b5bf99ee6d78e6c812a52
Arto Bendiken <arto@asimov.so>
2024-08-08 23:51:53 +0900

    Implement SysML traits in the derive macro.

--- lib/protoflow-derive/src/derives/function_block.rs
+++ lib/protoflow-derive/src/derives/function_block.rs
@@ -29,6 +29,29 @@ pub(crate) fn expand_derive_function_block(input: &DeriveInput) -> Result<TokenS
         _ => panic!("`#[derive(FunctionBlock)]` only supports structs"),
     };
 
+    #[cfg(not(feature = "sysml"))]
+    let impl_sysml_traits = quote! {};
+
+    #[cfg(feature = "sysml")]
+    let impl_sysml_traits = quote! {
+        #[automatically_derived]
+        #[allow(
+            unused_qualifications,
+            clippy::redundant_locals,
+        )]
+        impl #impl_generics #protoflow::prelude::sysml_model::BlockDefinition for #ident #ty_generics #where_clause {}
+        impl #impl_generics #protoflow::prelude::sysml_model::PartDefinition for #ident #ty_generics #where_clause {}
+        impl #impl_generics #protoflow::prelude::sysml_model::ItemDefinition for #ident #ty_generics #where_clause {}
+        impl #impl_generics #protoflow::prelude::sysml_model::OccurrenceDefinition for #ident #ty_generics #where_clause {}
+        impl #impl_generics #protoflow::prelude::sysml_model::Definition for #ident #ty_generics #where_clause {}
+        impl #impl_generics #protoflow::prelude::sysml_model::Structure for #ident #ty_generics #where_clause {}
+        impl #impl_generics #protoflow::prelude::sysml_model::Class for #ident #ty_generics #where_clause {}
+        impl #impl_generics #protoflow::prelude::sysml_model::Classifier for #ident #ty_generics #where_clause {}
+        impl #impl_generics #protoflow::prelude::sysml_model::Type for #ident #ty_generics #where_clause {}
+        impl #impl_generics #protoflow::prelude::sysml_model::Namespace for #ident #ty_generics #where_clause {}
+        impl #impl_generics #protoflow::prelude::sysml_model::Element for #ident #ty_generics #where_clause {}
+    };
+
     Ok(quote! {
         #[automatically_derived]
         #[allow(
@@ -63,5 +86,7 @@ pub(crate) fn expand_derive_function_block(input: &DeriveInput) -> Result<TokenS
                 Ok(())
             }
         }
+
+        #impl_sysml_traits
     })
 }
