594090128a0887c3acbae32402b3eb10263b05d7
Arto Bendiken <arto@asimov.so>
2024-08-08 17:25:33 +0900

    Define an optional SysML feature.

--- lib/protoflow/src/feature.rs
+++ lib/protoflow/src/feature.rs
@@ -8,6 +8,8 @@ pub static FEATURES: &[&str] = &[
     "flume",
     #[cfg(feature = "rand")]
     "rand",
+    #[cfg(feature = "sysml")]
+    "sysml",
     #[cfg(feature = "tokio")]
     "tokio",
     #[cfg(feature = "tracing")]
