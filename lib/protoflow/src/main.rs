7a9f72fb1ff98082cbc75ae5799ea2f8c90c07e9
Arto Bendiken <arto@asimov.so>
2024-08-11 01:49:30 +0900

    Sketch the CLI skeleton.

--- lib/protoflow/src/main.rs
+++ lib/protoflow/src/main.rs
@@ -1,7 +1,78 @@
 // This is free and unencumbered software released into the public domain.
 
-use clap::{Parser, Subcommand};
+mod sysexits;
 
-fn main() {
-    println!("Hello, world!");
+use crate::sysexits::Sysexits;
+use clap::{Parser, Subcommand};
+use dotenvy::dotenv;
+
+/// Protoflow command-line interface (CLI)
+#[derive(Debug, Parser)]
+#[command(name = "Protoflow", about)]
+#[command(arg_required_else_help = true)]
+struct Options {
+    /// Enable debugging output
+    #[clap(short = 'd', long, value_parser, global = true)]
+    debug: bool,
+
+    /// Show license information
+    #[clap(long, value_parser)]
+    license: bool,
+
+    /// Enable verbose output
+    #[clap(short = 'v', long, value_parser, global = true)]
+    verbose: bool,
+
+    /// Print version information
+    #[clap(short = 'V', long, value_parser)]
+    version: bool,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Show the current configuration
+    Config {},
+}
+
+pub fn main() -> Sysexits {
+    // Load environment variables from `.env`:
+    dotenv().ok();
+
+    // Expand wildcards and @argfiles:
+    let args = wild::args_os();
+    let args = argfile::expand_args_from(args, argfile::parse_fromfile, argfile::PREFIX).unwrap();
+
+    // Parse command-line options:
+    let options = Options::parse_from(args);
+
+    if options.version {
+        return version(&options).err().unwrap_or_default();
+    }
+
+    if options.license {
+        return license().err().unwrap_or_default();
+    }
+
+    if options.verbose || options.debug {
+        // TODO: configure tracing
+    }
+
+    let subcommand = &options.command;
+    let result = match subcommand.as_ref().expect("subcommand is required") {
+        Commands::Config {} => Ok(()),
+    };
+    return result.err().unwrap_or_default();
+}
+
+fn version(_options: &Options) -> Result<(), Sysexits> {
+    // TODO
+    Ok(())
+}
+
+fn license() -> Result<(), Sysexits> {
+    println!("This is free and unencumbered software released into the public domain.");
+    Ok(())
 }
---
+++ lib/protoflow/src/sysexits.rs
@@ -0,0 +1,80 @@
+// This is free and unencumbered software released into the public domain.
+
+#![allow(unused)]
+
+pub type Result = std::result::Result<(), Sysexits>;
+
+#[allow(non_camel_case_types, dead_code)]
+#[derive(Copy, Clone, Debug, Default)]
+#[repr(u8)]
+pub enum Sysexits {
+    #[default]
+    EX_OK = 0,
+    EX_USAGE = 64,
+    EX_DATAERR = 65,
+    EX_NOINPUT = 66,
+    EX_NOUSER = 67,
+    EX_NOHOST = 68,
+    EX_UNAVAILABLE = 69,
+    EX_SOFTWARE = 70,
+    EX_OSERR = 71,
+    EX_OSFILE = 72,
+    EX_CANTCREAT = 73,
+    EX_IOERR = 74,
+    EX_TEMPFAIL = 75,
+    EX_PROTOCOL = 76,
+    EX_NOPERM = 77,
+    EX_CONFIG = 78,
+}
+
+impl std::process::Termination for Sysexits {
+    fn report(self) -> std::process::ExitCode {
+        (self as u8).into()
+    }
+}
+
+impl From<std::boxed::Box<dyn std::error::Error>> for Sysexits {
+    fn from(_err: std::boxed::Box<dyn std::error::Error>) -> Self {
+        Sysexits::EX_SOFTWARE
+    }
+}
+
+impl From<std::io::Error> for Sysexits {
+    fn from(err: std::io::Error) -> Self {
+        use std::io::ErrorKind::*;
+        match err.kind() {
+            AddrInUse => Self::EX_TEMPFAIL,
+            AddrNotAvailable => Self::EX_USAGE,
+            AlreadyExists => Self::EX_CANTCREAT,
+            BrokenPipe => Self::EX_IOERR,
+            ConnectionAborted => Self::EX_PROTOCOL,
+            ConnectionRefused => Self::EX_UNAVAILABLE,
+            ConnectionReset => Self::EX_PROTOCOL,
+            Interrupted => Self::EX_TEMPFAIL,
+            InvalidData => Self::EX_DATAERR,
+            InvalidInput => Self::EX_DATAERR,
+            NotConnected => Self::EX_PROTOCOL,
+            NotFound => Self::EX_NOINPUT,
+            Other => Self::EX_UNAVAILABLE,
+            OutOfMemory => Self::EX_TEMPFAIL,
+            PermissionDenied => Self::EX_NOPERM,
+            TimedOut => Self::EX_IOERR,
+            UnexpectedEof => Self::EX_IOERR,
+            Unsupported => Self::EX_SOFTWARE,
+            WouldBlock => Self::EX_IOERR,
+            WriteZero => Self::EX_IOERR,
+            _ => Self::EX_UNAVAILABLE, // catch-all
+        }
+    }
+}
+
+pub fn exit(code: Sysexits) -> ! {
+    std::process::exit(code as i32);
+}
+
+macro_rules! abort {
+    ($code:expr, $($t:tt)*) => {{
+        std::eprintln!($($t)*);
+        exit($code)
+    }};
+}
