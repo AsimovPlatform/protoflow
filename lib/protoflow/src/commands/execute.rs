eff368254f84eed89550c5d1cb9db1962fad55bd
Arto Bendiken <arto@asimov.so>
2024-08-17 18:07:49 +0900

    Simplify system definitions using the new DSL.

--- lib/protoflow/src/commands/execute.rs
+++ lib/protoflow/src/commands/execute.rs
@@ -1,10 +1,11 @@
 // This is free and unencumbered software released into the public domain.
 
 use crate::sysexits::Sysexits;
-use protoflow_core::prelude::Bytes;
+use protoflow_blocks::{CoreBlocks, IoBlocks, SysBlocks, WriteEncoding};
+use protoflow_core::{SystemBuilding, SystemExecution};
 use std::path::PathBuf;
 
-type System = protoflow_core::System<protoflow_core::transports::MpscTransport>;
+type System = protoflow_blocks::System;
 
 #[derive(Debug)]
 pub enum ExecuteError {
@@ -14,22 +15,29 @@ pub enum ExecuteError {
 }
 
 pub fn execute(block: &PathBuf, params: &Vec<(String, String)>) -> Result<(), Sysexits> {
-    let path_or_uri = block.to_string_lossy();
+    let path_or_uri = block.to_string_lossy().to_string();
     let system = match path_or_uri.as_ref() {
+        "Buffer" => todo!(),
         "Const" => {
-            use protoflow_blocks::{Const, WriteStdout};
-            let Some(value) = params.iter().find(|(k, _)| k == "value").map(|(_, v)| v) else {
+            let Some(value) = params
+                .iter()
+                .find(|(k, _)| k == "value")
+                .map(|(_, v)| v.clone())
+            else {
                 return Err(ExecuteError::MissingParameter("value"))?;
             };
-            let value = Bytes::from(value.clone());
             System::build(|s| {
-                let r#const = s.block(Const::with_params(s.output(), value));
-                let stdout = s.block(WriteStdout::new(s.input()));
-                s.connect(&r#const.output, &stdout.input);
+                let value = s.const_string(value);
+                let encoder = s.encode_with(WriteEncoding::TextWithNewlineSuffix);
+                let stdout = s.write_stdout();
+                s.connect(&value.output, &encoder.input);
+                s.connect(&encoder.output, &stdout.input);
             })
         }
+        "Count" => todo!(),
+        "Delay" => todo!(),
+        "Drop" => todo!(),
         "Random" => {
-            use protoflow_blocks::{Random, Write, WriteStdout};
             let seed = params
                 .iter()
                 .find(|(k, _)| k == "seed")
@@ -39,27 +47,26 @@ pub fn execute(block: &PathBuf, params: &Vec<(String, String)>) -> Result<(), Sy
             }
             let seed = seed.map(Result::unwrap);
             System::build(|s| {
-                let random = s.block(Random::<u64>::with_params(s.output(), seed));
-                let adapter = s.block(Write::<u64>::new(s.input(), s.output()));
-                let stdout = s.block(WriteStdout::new(s.input()));
-                s.connect(&random.output, &adapter.input);
-                s.connect(&adapter.output, &stdout.input);
+                let random = s.random::<u64>(seed);
+                let encoder = s.encode_with::<u64>(WriteEncoding::TextWithNewlineSuffix);
+                let stdout = s.write_stdout();
+                s.connect(&random.output, &encoder.input);
+                s.connect(&encoder.output, &stdout.input);
             })
         }
         "ReadEnv" => {
-            use protoflow_blocks::{Const, ReadEnv, Write, WriteStdout};
             let Some(name) = params.iter().find(|(k, _)| k == "name").map(|(_, v)| v) else {
                 return Err(ExecuteError::MissingParameter("name"))?;
             };
             let name = name.clone();
             System::build(|s| {
-                let name = s.block(Const::<String>::with_params(s.output(), name));
-                let env = s.block(ReadEnv::<String>::new(s.input(), s.output()));
-                let adapter = s.block(Write::<String>::new(s.input(), s.output()));
-                let stdout = s.block(WriteStdout::new(s.input()));
+                let name = s.const_string(name);
+                let env = s.read_env();
+                let encoder = s.encode_with(WriteEncoding::TextWithNewlineSuffix);
+                let stdout = s.write_stdout();
                 s.connect(&name.output, &env.name);
-                s.connect(&env.output, &adapter.input);
-                s.connect(&adapter.output, &stdout.input);
+                s.connect(&env.output, &encoder.input);
+                s.connect(&encoder.output, &stdout.input);
             })
         }
         _ => return Err(ExecuteError::UnknownSystem(path_or_uri.to_string()))?,
