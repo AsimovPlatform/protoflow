93dfb75ccf0943fcec5fb3ee855a468a9dd09870
Arto Bendiken <arto@asimov.so>
2024-08-18 10:37:44 +0900

    Implement `protoflow execute Count`, etc.

--- lib/protoflow-blocks/src/io.rs
+++ lib/protoflow-blocks/src/io.rs
@@ -9,8 +9,17 @@ use protoflow_core::Message;
 pub trait IoBlocks {
     fn decode<T: Message + FromStr + 'static>(&self) -> Decode<T>;
     fn decode_with<T: Message + FromStr + 'static>(&self, encoding: Encoding) -> Decode<T>;
+
+    fn decode_lines<T: Message + FromStr + 'static>(&self) -> Decode<T> {
+        self.decode_with::<T>(Encoding::TextWithNewlineSuffix)
+    }
+
     fn encode<T: Message + ToString + 'static>(&self) -> Encode<T>;
     fn encode_with<T: Message + ToString + 'static>(&self, encoding: Encoding) -> Encode<T>;
+
+    fn encode_lines<T: Message + ToString + 'static>(&self) -> Encode<T> {
+        self.encode_with::<T>(Encoding::TextWithNewlineSuffix)
+    }
 }
 
 mod decode;
--- lib/protoflow/src/commands/execute.rs
+++ lib/protoflow/src/commands/execute.rs
@@ -1,7 +1,7 @@
 // This is free and unencumbered software released into the public domain.
 
 use crate::sysexits::Sysexits;
-use protoflow_blocks::{CoreBlocks, DelayType, Encoding, IoBlocks, SysBlocks};
+use protoflow_blocks::{CoreBlocks, DelayType, IoBlocks, SysBlocks};
 use protoflow_core::{SystemBuilding, SystemExecution};
 use std::{path::PathBuf, time::Duration};
 
@@ -27,14 +27,24 @@ pub fn execute(block: &PathBuf, params: &Vec<(String, String)>) -> Result<(), Sy
                 return Err(ExecuteError::MissingParameter("value"))?;
             };
             System::build(|s| {
-                let value = s.const_string(value);
-                let encoder = s.encode_with(Encoding::TextWithNewlineSuffix);
+                let const_source = s.const_string(value);
+                let line_encoder = s.encode_lines();
                 let stdout = s.write_stdout();
-                s.connect(&value.output, &encoder.input);
-                s.connect(&encoder.output, &stdout.input);
+                s.connect(&const_source.output, &line_encoder.input);
+                s.connect(&line_encoder.output, &stdout.input);
             })
         }
-        "Count" => todo!(),
+        "Count" => System::build(|s| {
+            let stdin = s.read_stdin();
+            let line_decoder = s.decode_lines::<String>(); // TODO
+            let counter = s.count();
+            let line_encoder = s.encode_lines();
+            let stdout = s.write_stdout();
+            s.connect(&stdin.output, &line_decoder.input);
+            s.connect(&line_decoder.output, &counter.input);
+            s.connect(&counter.count, &line_encoder.input);
+            s.connect(&line_encoder.output, &stdout.input);
+        }),
         "Delay" => {
             let fixed_delay = params
                 .iter()
@@ -46,21 +56,21 @@ pub fn execute(block: &PathBuf, params: &Vec<(String, String)>) -> Result<(), Sy
             let fixed_delay = fixed_delay.map(Result::unwrap);
             let delay = DelayType::Fixed(Duration::from_secs_f64(fixed_delay.unwrap()));
             System::build(|s| {
-                let random = s.random::<u64>(None);
-                let delay = s.delay_by(delay);
-                let encoder = s.encode_with::<u64>(Encoding::TextWithNewlineSuffix);
+                let random_source = s.random::<u64>(None);
+                let delayer = s.delay_by(delay);
+                let line_encoder = s.encode_lines::<u64>();
                 let stdout = s.write_stdout();
-                s.connect(&random.output, &delay.input);
-                s.connect(&delay.output, &encoder.input);
-                s.connect(&encoder.output, &stdout.input);
+                s.connect(&random_source.output, &delayer.input);
+                s.connect(&delayer.output, &line_encoder.input);
+                s.connect(&line_encoder.output, &stdout.input);
             })
         }
         "Drop" => System::build(|s| {
             let stdin = s.read_stdin();
-            let decoder = s.decode_with::<String>(Encoding::TextWithNewlineSuffix); // TODO
-            let drop = s.drop();
-            s.connect(&stdin.output, &decoder.input);
-            s.connect(&decoder.output, &drop.input);
+            let line_decoder = s.decode_lines::<String>(); // TODO
+            let dropper = s.drop();
+            s.connect(&stdin.output, &line_decoder.input);
+            s.connect(&line_decoder.output, &dropper.input);
         }),
         "Random" => {
             let seed = params
@@ -72,11 +82,11 @@ pub fn execute(block: &PathBuf, params: &Vec<(String, String)>) -> Result<(), Sy
             }
             let seed = seed.map(Result::unwrap);
             System::build(|s| {
-                let random = s.random::<u64>(seed);
-                let encoder = s.encode_with::<u64>(Encoding::TextWithNewlineSuffix);
+                let random_source = s.random::<u64>(seed);
+                let line_encoder = s.encode_lines::<u64>();
                 let stdout = s.write_stdout();
-                s.connect(&random.output, &encoder.input);
-                s.connect(&encoder.output, &stdout.input);
+                s.connect(&random_source.output, &line_encoder.input);
+                s.connect(&line_encoder.output, &stdout.input);
             })
         }
         "ReadEnv" => {
@@ -85,13 +95,13 @@ pub fn execute(block: &PathBuf, params: &Vec<(String, String)>) -> Result<(), Sy
             };
             let name = name.clone();
             System::build(|s| {
-                let name = s.const_string(name);
-                let env = s.read_env();
-                let encoder = s.encode_with(Encoding::TextWithNewlineSuffix);
+                let name_param = s.const_string(name);
+                let env_reader = s.read_env();
+                let line_encoder = s.encode_lines();
                 let stdout = s.write_stdout();
-                s.connect(&name.output, &env.name);
-                s.connect(&env.output, &encoder.input);
-                s.connect(&encoder.output, &stdout.input);
+                s.connect(&name_param.output, &env_reader.name);
+                s.connect(&env_reader.output, &line_encoder.input);
+                s.connect(&line_encoder.output, &stdout.input);
             })
         }
         "ReadDir" => {
@@ -103,15 +113,25 @@ pub fn execute(block: &PathBuf, params: &Vec<(String, String)>) -> Result<(), Sy
                 return Err(ExecuteError::MissingParameter("path"))?;
             };
             System::build(|s| {
-                let path = s.const_string(path);
-                let dir = s.read_dir();
-                let encoder = s.encode_with(Encoding::TextWithNewlineSuffix);
+                let path_param = s.const_string(path);
+                let dir_reader = s.read_dir();
+                let line_encoder = s.encode_lines();
                 let stdout = s.write_stdout();
-                s.connect(&path.output, &dir.path);
-                s.connect(&dir.output, &encoder.input);
-                s.connect(&encoder.output, &stdout.input);
+                s.connect(&path_param.output, &dir_reader.path);
+                s.connect(&dir_reader.output, &line_encoder.input);
+                s.connect(&line_encoder.output, &stdout.input);
             })
         }
+        "ReadStdin" | "WriteStdout" => System::build(|s| {
+            let stdin = s.read_stdin();
+            let stdout = s.write_stdout();
+            s.connect(&stdin.output, &stdout.input);
+        }),
+        "WriteStderr" => System::build(|s| {
+            let stdin = s.read_stdin();
+            let stderr = s.write_stderr();
+            s.connect(&stdin.output, &stderr.input);
+        }),
         _ => return Err(ExecuteError::UnknownSystem(path_or_uri.to_string()))?,
     };
     system.execute().unwrap().join().unwrap(); // TODO
