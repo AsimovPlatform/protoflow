936145bf7123ec424c582a8bd65f52e3e3bc726f
Arto Bendiken <arto@asimov.so>
2024-08-19 20:08:02 +0900

    Disable experimental `#[doc(cfg)]` attributes, for now.
    See: https://github.com/rust-lang/rust/issues/43781

--- lib/protoflow/src/lib.rs
+++ lib/protoflow/src/lib.rs
@@ -13,13 +13,13 @@ pub use protoflow_core::*;
 /// Default blocks are available if the crate was built with a
 /// `features = ["blocks"]` configuration.
 #[cfg(feature = "blocks")]
-#[cfg_attr(docsrs, doc(cfg(feature = "blocks")))]
+//#[cfg_attr(docsrs, doc(cfg(feature = "blocks")))]
 pub use protoflow_blocks as blocks;
 
 /// Derive macros are available if the crate was built with a
 /// `features = ["derive"]` configuration.
 #[cfg(feature = "derive")]
-#[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
+//#[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
 pub use protoflow_derive as derive;
 
 mod feature;
@@ -28,7 +28,7 @@ pub use feature::*;
 /// The parser is available if the crate was built with a
 /// `features = ["syntax"]` configuration.
 #[cfg(feature = "syntax")]
-#[cfg_attr(docsrs, doc(cfg(feature = "syntax")))]
+//#[cfg_attr(docsrs, doc(cfg(feature = "syntax")))]
 pub use protoflow_syntax as syntax;
 
 #[doc = include_str!("../../../README.md")]
