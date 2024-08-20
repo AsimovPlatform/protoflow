24fbf27774d3d6da0b356c5e73ed68af87cd8be7
Arto Bendiken <arto@asimov.so>
2024-08-20 20:13:54 +0900

    Begin the MPSC transport rewrite.

--- lib/protoflow-core/src/transports/mpsc.rs
+++ lib/protoflow-core/src/transports/mpsc.rs
@@ -3,27 +3,34 @@
 extern crate std;
 
 use crate::{
-    prelude::{Bytes, ToString, Vec},
+    prelude::{Bytes, ToString},
     transport::Transport,
     InputPortID, OutputPortID, PortError, PortID, PortResult, PortState,
 };
-use parking_lot::RwLock;
+use parking_lot::{Mutex, RwLock};
+use sharded_slab::Slab;
 use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
 
-pub(crate) const DEFAULT_INPUT_PORT_COUNT: usize = 16;
-pub(crate) const DEFAULT_OUTPUT_PORT_COUNT: usize = 16;
+//pub(crate) const DEFAULT_INPUT_PORT_COUNT: usize = 16;
+//pub(crate) const DEFAULT_OUTPUT_PORT_COUNT: usize = 16;
 pub(crate) const DEFAULT_CONNECTION_CAPACITY: usize = 1;
 
 #[derive(Debug, Default)]
 pub struct MpscTransport {
-    pub state: RwLock<MpscTransportState>,
+    outputs: Slab<MpscTransportOutputPort>,
+    inputs: Slab<MpscTransportInputPort>,
 }
 
 #[derive(Debug, Default)]
-pub struct MpscTransportState {
-    outputs: Vec<PortState>,
-    inputs: Vec<PortState>,
-    channels: Vec<(SyncSender<MpscTransportEvent>, Receiver<MpscTransportEvent>)>,
+pub struct MpscTransportOutputPort {
+    state: RwLock<PortState>,
+}
+
+#[derive(Debug)]
+pub struct MpscTransportInputPort {
+    state: RwLock<PortState>,
+    receiver: Mutex<Receiver<MpscTransportEvent>>,
+    sender: SyncSender<MpscTransportEvent>,
 }
 
 #[derive(Debug)]
@@ -33,173 +40,177 @@ pub enum MpscTransportEvent {
     Disconnect,
 }
 
-unsafe impl Sync for MpscTransportState {}
-
 impl MpscTransport {
     pub fn new() -> Self {
-        Self {
-            state: RwLock::new(MpscTransportState::new()),
-        }
-    }
-}
-
-impl MpscTransportState {
-    pub fn new() -> Self {
-        // Avoid reallocations by pre-allocating an ample default capacity.
-        Self {
-            outputs: Vec::with_capacity(DEFAULT_OUTPUT_PORT_COUNT),
-            inputs: Vec::with_capacity(DEFAULT_INPUT_PORT_COUNT),
-            channels: Vec::with_capacity(DEFAULT_INPUT_PORT_COUNT),
-        }
+        Self::default()
     }
 }
 
 impl Transport for MpscTransport {
     fn state(&self, port: PortID) -> PortResult<PortState> {
-        let state = self.state.read();
         match port {
-            PortID::Input(input) => match state.inputs.get(input.index()) {
+            PortID::Input(input) => match self.inputs.get(input.index()) {
                 None => Err(PortError::Invalid(port)),
-                Some(state) => Ok(*state),
+                Some(entry) => Ok(*entry.state.read()),
             },
-            PortID::Output(output) => match state.outputs.get(output.index()) {
+            PortID::Output(output) => match self.outputs.get(output.index()) {
                 None => Err(PortError::Invalid(port)),
-                Some(state) => Ok(*state),
+                Some(entry) => Ok(*entry.state.read()),
             },
         }
     }
 
     fn open_input(&self) -> PortResult<InputPortID> {
-        let mut state = self.state.write();
-        state.inputs.push(PortState::Open);
-        state
-            .channels
-            .push(sync_channel(DEFAULT_CONNECTION_CAPACITY));
+        let (sender, receiver) = sync_channel(DEFAULT_CONNECTION_CAPACITY);
 
-        InputPortID::try_from(-(state.inputs.len() as isize))
+        let input_index = self
+            .inputs
+            .insert(MpscTransportInputPort {
+                state: RwLock::new(PortState::Open),
+                receiver: Mutex::new(receiver),
+                sender,
+            })
+            .unwrap();
+
+        InputPortID::try_from(-(input_index as isize + 1))
             .map_err(|s| PortError::Other(s.to_string()))
     }
 
     fn open_output(&self) -> PortResult<OutputPortID> {
-        let mut state = self.state.write();
-        state.outputs.push(PortState::Open);
+        let output_index = self
+            .outputs
+            .insert(MpscTransportOutputPort {
+                state: RwLock::new(PortState::Open),
+            })
+            .unwrap();
 
-        OutputPortID::try_from(state.outputs.len() as isize)
+        OutputPortID::try_from(output_index as isize + 1)
             .map_err(|s| PortError::Other(s.to_string()))
     }
 
     fn close_input(&self, input: InputPortID) -> PortResult<bool> {
         let input_index = input.index();
-        let mut state = self.state.upgradable_read();
-        Ok(match state.inputs.get(input_index) {
-            None => return Err(PortError::Invalid(input.into())),
-            Some(input_state) => match input_state {
-                PortState::Closed => false, // already closed
-                PortState::Open => {
-                    state.with_upgraded(|state| {
-                        state.inputs[input_index] = PortState::Closed;
-                    });
-                    true
-                }
-                PortState::Connected(PortID::Output(output)) => {
-                    let output_index = output.index();
-                    debug_assert!(matches!(
-                        state.outputs[output_index],
-                        PortState::Connected(PortID::Input(_))
-                    ));
-                    let sender = state.channels[input_index].0.clone();
-                    state.with_upgraded(|state| {
-                        state.outputs[output_index] = PortState::Open;
-                        state.inputs[input_index] = PortState::Closed;
-                    });
-                    drop(state);
-                    sender.send(MpscTransportEvent::Disconnect).unwrap(); // blocking
-                    true
-                }
-                PortState::Connected(PortID::Input(_)) => unreachable!(),
-            },
+        let Some(input_entry) = self.inputs.get(input_index) else {
+            return Err(PortError::Invalid(input.into()));
+        };
+
+        let input_state = *input_entry.state.read();
+        Ok(match input_state {
+            PortState::Closed => false, // already closed
+            PortState::Open => {
+                *input_entry.state.write() = PortState::Closed;
+                true
+            }
+            PortState::Connected(PortID::Output(output)) => {
+                let output_index = output.index();
+                let Some(output_entry) = self.outputs.get(output_index) else {
+                    return Err(PortError::Invalid(output.into()));
+                };
+                debug_assert!(matches!(
+                    *output_entry.state.read(),
+                    PortState::Connected(PortID::Input(_))
+                ));
+                let sender = input_entry.sender.clone();
+                *output_entry.state.write() = PortState::Open;
+                *input_entry.state.write() = PortState::Closed;
+                sender.send(MpscTransportEvent::Disconnect).unwrap(); // blocking
+                true
+            }
+            PortState::Connected(PortID::Input(_)) => unreachable!(),
         })
     }
 
     fn close_output(&self, output: OutputPortID) -> PortResult<bool> {
         let output_index = output.index();
-        let mut state = self.state.upgradable_read();
-        Ok(match state.outputs.get(output_index) {
-            None => return Err(PortError::Invalid(output.into())),
-            Some(output_state) => match output_state {
-                PortState::Closed => false, // already closed
-                PortState::Open => {
-                    state.with_upgraded(|state| {
-                        state.outputs[output_index] = PortState::Closed;
-                    });
-                    true
-                }
-                PortState::Connected(PortID::Input(input)) => {
-                    let input = input.clone();
-                    let input_index = input.index();
-                    debug_assert!(matches!(
-                        state.inputs[input_index],
-                        PortState::Connected(PortID::Output(_))
-                    ));
-                    let sender = state.channels[input_index].0.clone();
-                    sender.send(MpscTransportEvent::Disconnect).unwrap(); // blocking
-                    state.with_upgraded(|state| {
-                        state.outputs[output_index] = PortState::Closed;
-                        state.inputs[input_index] = PortState::Open;
-                    });
-                    drop(state);
-                    true
-                }
-                PortState::Connected(PortID::Output(_)) => unreachable!(),
-            },
+        let Some(output_entry) = self.outputs.get(output_index) else {
+            return Err(PortError::Invalid(output.into()));
+        };
+
+        let output_state = *output_entry.state.read();
+        Ok(match output_state {
+            PortState::Closed => false, // already closed
+            PortState::Open => {
+                *output_entry.state.write() = PortState::Closed;
+                true
+            }
+            PortState::Connected(PortID::Input(input)) => {
+                let input = input.clone();
+                let input_index = input.index();
+                let Some(input_entry) = self.inputs.get(input_index) else {
+                    return Err(PortError::Invalid(input.into()));
+                };
+                debug_assert!(matches!(
+                    *input_entry.state.read(),
+                    PortState::Connected(PortID::Output(_))
+                ));
+                let sender = input_entry.sender.clone();
+                sender.send(MpscTransportEvent::Disconnect).unwrap(); // blocking
+                *output_entry.state.write() = PortState::Closed;
+                *input_entry.state.write() = PortState::Open;
+                true
+            }
+            PortState::Connected(PortID::Output(_)) => unreachable!(),
         })
     }
 
     fn connect(&self, source: OutputPortID, target: InputPortID) -> PortResult<bool> {
-        let mut state = self.state.write();
-        match (
-            state.outputs.get(source.index()),
-            state.inputs.get(target.index()),
-        ) {
-            (Some(PortState::Open), Some(PortState::Open)) => {
-                state.outputs[source.index()] = PortState::Connected(PortID::Input(target));
-                state.inputs[target.index()] = PortState::Connected(PortID::Output(source));
-            }
-            _ => return Err(PortError::Invalid(PortID::Output(source))), // TODO: better errors
+        let Some(output_entry) = self.outputs.get(source.index()) else {
+            return Err(PortError::Invalid(source.into()));
         };
+
+        let Some(input_entry) = self.inputs.get(target.index()) else {
+            return Err(PortError::Invalid(target.into()));
+        };
+
+        let output_state = *output_entry.state.read();
+        let input_state = *input_entry.state.read();
+        match (output_state, input_state) {
+            (PortState::Open, PortState::Open) => {
+                // FIXME: this has multiple race conditions right now:
+                *output_entry.state.write() = PortState::Connected(PortID::Input(target));
+                *input_entry.state.write() = PortState::Connected(PortID::Output(source));
+            }
+            _ => return Err(PortError::Invalid(source.into())), // TODO: better errors
+        };
+
         Ok(true)
     }
 
     fn send(&self, output: OutputPortID, message: Bytes) -> PortResult<()> {
-        let state = self.state.read();
-        let input = {
-            match state.outputs.get(output.index()) {
-                None => return Err(PortError::Invalid(PortID::Output(output))),
-                Some(PortState::Closed) => return Err(PortError::Closed),
-                Some(PortState::Open) => return Err(PortError::Disconnected),
-                Some(PortState::Connected(PortID::Output(_))) => unreachable!(),
-                Some(PortState::Connected(PortID::Input(input))) => *input,
-            }
+        let Some(output_entry) = self.outputs.get(output.index()) else {
+            return Err(PortError::Invalid(output.into()));
         };
-        let sender = state.channels[input.index()].0.clone();
-        drop(state);
+
+        let input = match *output_entry.state.read() {
+            PortState::Closed => return Err(PortError::Closed),
+            PortState::Open => return Err(PortError::Disconnected),
+            PortState::Connected(PortID::Output(_)) => unreachable!(),
+            PortState::Connected(PortID::Input(input)) => input,
+        };
+        let Some(input_entry) = self.inputs.get(input.index()) else {
+            return Err(PortError::Invalid(input.into()));
+        };
+
+        let sender = input_entry.sender.clone();
         Ok(sender.send(MpscTransportEvent::Message(message)).unwrap()) // blocking (TODO: error handling)
     }
 
     fn recv(&self, input: InputPortID) -> PortResult<Option<Bytes>> {
-        let state = self.state.read();
-        if state.inputs.get(input.index()).is_none() {
-            return Err(PortError::Invalid(PortID::Input(input)));
-        }
-        if state.inputs[input.index()].is_closed() {
+        let Some(input_entry) = self.inputs.get(input.index()) else {
+            return Err(PortError::Invalid(input.into()));
+        };
+
+        if input_entry.state.read().is_closed() {
             return Ok(None); // EOS
         }
-        let receiver = &state.channels[input.index()].1;
-        let event = receiver
-            .recv() // blocking
-            .map_err(|_| PortError::Disconnected)?;
+
+        let receiver = input_entry.receiver.lock();
+
         use MpscTransportEvent::*;
-        match event {
+        match receiver
+            .recv() // blocking
+            .map_err(|_| PortError::Disconnected)?
+        {
             Connect => unreachable!(),
             Message(bytes) => Ok(Some(bytes)),
             Disconnect => Ok(None), // EOS
