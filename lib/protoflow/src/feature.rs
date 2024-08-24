6d6789e0f7793532bccd9f27e154494877ecb91c
Arto Bendiken <arto@asimov.so>
2024-08-25 00:44:31 +0900

    Expose the Serde feature in the metacrate as well.

--- lib/protoflow/src/feature.rs
+++ lib/protoflow/src/feature.rs
@@ -14,6 +14,8 @@ pub static FEATURES: &[&str] = &[
     "flume",
     #[cfg(feature = "rand")]
     "rand",
+    #[cfg(feature = "serde")]
+    "serde",
     #[cfg(feature = "syntax")]
     "syntax",
     #[cfg(feature = "sysml")]
