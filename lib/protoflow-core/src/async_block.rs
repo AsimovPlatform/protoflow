f35c63eecb5f3009d4d1b38e0668e4e56cbf46dd
Yura Menshov <user344u@gmail.com>
2024-10-07 15:00:17 +0300

    Implement BlockType, AsyncBlock & async methods.

--- lib/protoflow-core/src/async_block.rs
+++ lib/protoflow-core/src/async_block.rs
@@ -1,9 +1,10 @@
 // This is free and unencumbered software released into the public domain.
 
-use async_trait::async_trait;
-
 use crate::prelude::Box;
 use crate::{BlockDescriptor, BlockHooks, BlockResult, BlockRuntime};
+
+use async_trait::async_trait;
+use core::fmt;
 
 /// An async version of a regular block.
 #[async_trait]
@@ -18,4 +19,16 @@ pub trait AsyncBlock: BlockDescriptor + BlockHooks + Send + Sync {
 
     /// Executes this block's computation asynchronously.
     async fn execute_async(&mut self, runtime: &dyn BlockRuntime) -> BlockResult;
+}
+
+impl fmt::Debug for dyn AsyncBlock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("AsyncBlock")
+            //.field("name", &self.name())
+            //.field("label", &self.label())
+            .field("inputs", &self.inputs())
+            .field("outputs", &self.outputs())
+            .field("parameters", &self.parameters())
+            .finish()
+    }
 }
