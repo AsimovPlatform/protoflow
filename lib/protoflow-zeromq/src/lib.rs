838bdebc82d0a1e2d01cf4729f4aaeb8f39b9052
Samuel Sarle <samuel@sarle.com>
2024-11-18 19:26:06 +0200

    Add rudimentary `send` and `recv`

--- lib/protoflow-zeromq/src/lib.rs
+++ lib/protoflow-zeromq/src/lib.rs
@@ -8,27 +8,67 @@ pub use protoflow_core::prelude;
 
 extern crate std;
 
-use protoflow_core::{prelude::Bytes, InputPortID, OutputPortID, PortResult, PortState, Transport};
+use protoflow_core::{
+    prelude::{BTreeMap, Bytes},
+    InputPortID, OutputPortID, PortError, PortResult, PortState, Transport,
+};
 
-use zeromq::{Socket, SocketRecv, SocketSend};
+use parking_lot::{Mutex, RwLock};
+use std::sync::mpsc::{Receiver, SyncSender};
+use zeromq::{util::PeerIdentity, Socket, SocketOptions, SocketRecv, SocketSend};
 
 pub struct ZMQTransport {
-    psock: zeromq::PubSocket,
-    ssock: zeromq::SubSocket,
+    sock: Mutex<zeromq::RouterSocket>,
     tokio: tokio::runtime::Handle,
+    outputs: BTreeMap<OutputPortID, RwLock<ZmqOutputPortState>>,
+    inputs: BTreeMap<InputPortID, RwLock<ZmqInputPortState>>,
+}
+
+#[derive(Debug, Clone, Default)]
+enum ZmqOutputPortState {
+    #[default]
+    Open,
+    Connected(SyncSender<ZmqTransportEvent>),
+    Closed,
+}
+
+#[derive(Debug, Default)]
+enum ZmqInputPortState {
+    #[default]
+    Open,
+    Connected(Mutex<Receiver<ZmqTransportEvent>>),
+    Closed,
+}
+
+#[derive(Clone, Debug)]
+enum ZmqTransportEvent {
+    Connect,
+    Message(Bytes),
+    Disconnect,
 }
 
 impl ZMQTransport {
     pub fn new(url: &str) -> Self {
         let tokio = tokio::runtime::Handle::current();
-        let mut psock = zeromq::PubSocket::new();
-        tokio.block_on(psock.connect(url)).expect("psock conn");
-        let mut ssock = zeromq::SubSocket::new();
-        tokio.block_on(ssock.connect(url)).expect("ssock conn");
+
+        let peer_id = PeerIdentity::new();
+        let mut sock_opts = SocketOptions::default();
+        sock_opts.peer_identity(peer_id);
+
+        let mut sock = zeromq::RouterSocket::with_options(sock_opts);
+        tokio
+            .block_on(sock.connect(url))
+            .expect("failed to connect");
+        let sock = Mutex::new(sock);
+
+        let outputs = BTreeMap::default();
+        let inputs = BTreeMap::default();
+
         Self {
-            psock,
-            ssock,
+            sock,
             tokio,
+            outputs,
+            inputs,
         }
     }
 }
@@ -63,11 +103,40 @@ impl Transport for ZMQTransport {
     }
 
     fn send(&self, output: OutputPortID, message: Bytes) -> PortResult<()> {
-        todo!();
+        let Some(output_state) = self.outputs.get(&output) else {
+            todo!();
+        };
+
+        use ZmqOutputPortState::*;
+        match *output_state.read() {
+            Open => todo!(),
+            Closed => todo!(),
+            Connected(ref sender) => {
+                let msg = ZmqTransportEvent::Message(message);
+                Ok(sender.send(msg).unwrap())
+            }
+        }
     }
 
     fn recv(&self, input: InputPortID) -> PortResult<Option<Bytes>> {
-        todo!();
+        let Some(input_state) = self.inputs.get(&input) else {
+            todo!();
+        };
+
+        use ZmqInputPortState::*;
+        match *input_state.read() {
+            Open => todo!(),
+            Closed => todo!(),
+            Connected(ref receiver) => {
+                use ZmqTransportEvent::*;
+                let receiver = receiver.lock();
+                match receiver.recv().map_err(|_| PortError::Disconnected)? {
+                    Connect => todo!(),
+                    Disconnect => todo!(),
+                    Message(bytes) => Ok(Some(bytes)),
+                }
+            }
+        }
     }
 
     fn try_recv(&self, _input: InputPortID) -> PortResult<Option<Bytes>> {
