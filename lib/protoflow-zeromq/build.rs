c133cfd67d9ec37ff74b11337fc053a481617838
Samuel Sarle <samuel@sarle.com>
2024-12-05 12:48:13 +0200

    Separate proto and rust files

--- lib/protoflow-zeromq/build.rs
+++ lib/protoflow-zeromq/build.rs
@@ -2,5 +2,5 @@ use std::io::Result;
 fn main() -> Result<()> {
     prost_build::Config::default()
         .out_dir("src/")
-        .compile_protos(&["src/transport_event.proto"], &["src/"])
+        .compile_protos(&["proto/transport_event.proto"], &["proto/"])
 }
