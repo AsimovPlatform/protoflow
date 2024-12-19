9845faa19f742f6a3322281c7cfbd246c7c6c0ee
Yura Menshov <user344u@gmail.com>
2024-12-19 13:33:54 +0200

    change example

--- lib/protoflow-blocks/src/blocks/flow/gate.rs
+++ lib/protoflow-blocks/src/blocks/flow/gate.rs
@@ -28,8 +28,14 @@ use simple_mermaid::mermaid;
 /// # fn main() {
 /// System::build(|s| {
 ///     let stdin = s.read_stdin();
+///     let hello = s.const_string("Hello, World!");
+///     let encode = s.encode_lines();
 ///     let gate = s.gate();
-///     s.connect(&stdin.output, &gate.input);
+///     let stdout = s.write_stdout();
+///     s.connect(&hello.output, &encode.input);
+///     s.connect(&encode.output, &gate.input);
+///     s.connect(&stdin.output, &gate.trigger);
+///     s.connect(&gate.output, &stdout.input);
 /// });
 /// # }
 /// ```
